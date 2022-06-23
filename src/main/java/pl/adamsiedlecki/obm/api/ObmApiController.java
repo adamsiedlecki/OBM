c91aeba4c5eadba246aecc1e5214fa1f5249984e
adam <adamsiedlec@wp.pl>
2022-06-23 23:49:23 +0200

    Initial commit

---
+++ src/main/java/pl/adamsiedlecki/obm/api/ObmApiController.java
@@ -0,0 +1,13 @@
+package pl.adamsiedlecki.obm.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class ObmApiController {
+
+    public void receiveBroadcastInfo() {
+
+    }
+}
---
+++ src/test/java/pl/adamsiedlecki/obm/ObmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adamsiedlecki.obm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
